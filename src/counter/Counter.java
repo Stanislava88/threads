200c265c4a90890f5e3299a2207e44db0133e422
KrasimirRaikov <raikov.krasimir@gmail.com>
2015-12-16 17:41:16 +0200

    first task counter
    the thread counts until it is interupted by a terminal input
    then on the screen is printed the last value of the thread

---
+++ src/counter/Counter.java
@@ -0,0 +1,44 @@
+package counter;
+
+import java.util.Scanner;
+
+/**
+ * @author Krasimir Raikov(raikov.krasimir@gmail.com)
+ */
+public class Counter implements Runnable {
+
+
+    private static int currentValue;
+    private int value;
+
+    public Counter(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            for(int i=0; i<value;i++){
+                currentValue=i;
+                Thread.sleep(100);
+
+            }
+        } catch (InterruptedException e) {
+            System.out.println("thread stoped");
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread=new Thread(new Counter(1000));
+        thread.start();
+        Scanner input= new Scanner(System.in);
+        while (thread.isAlive()){
+            input.next();
+            thread.interrupt();
+            System.out.println(currentValue);
+        }
+
+    }
+}
